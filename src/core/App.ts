80c7fcaafa00d41f7751ccfc4d63a0d89569d518
proyavin <pasha.proyavin@gmail.com>
2020-11-18 21:12:46 +0300

    refactoring drag

--- src/core/App.ts
+++ src/core/App.ts
@@ -10,13 +10,13 @@ export class App {
     public FPSFinal = 0;
 
     constructor(id: string) {
-        this.element = <HTMLCanvasElement>document.getElementById(id);
-        this.ctx = <CanvasRenderingContext2D>this.element.getContext('2d');
+        this.element = (document.getElementById(id) as HTMLCanvasElement);
+        this.ctx = (this.element.getContext('2d') as CanvasRenderingContext2D);
 
         setInterval(() => {
             this.FPSFinal = this.FPS;
             this.FPS = 0;
-        }, 1000)
+        }, 1000);
 
     }
 
@@ -25,9 +25,61 @@ export class App {
         this.drawBackground();
 
         this.containers.push(new Container(50, 50, 100, 200, this.ctx));
+        this.containers.push(new Container(350, 250, 100, 200, this.ctx));
 
         window.requestAnimationFrame(() => {
             this.draw();
+        });
+
+        let cc = 0;
+        let isDown = false;
+        let oldMouseX = 0;
+        let oldMouseY = 0;
+        let oldThisX = 0;
+        let oldThisY = 0;
+
+        this.element.addEventListener('mousedown', (event) => {
+            cc = this.containers.findIndex((container) => {
+                return (event.x >= container.x && event.x <= container.x + container.width) &&
+                    (event.y >= container.y && event.y <= container.y + container.height);
+            });
+
+            if (cc !== -1) {
+                isDown = true;
+                oldMouseX = event.x;
+                oldMouseY = event.y;
+
+                oldThisX = this.containers[cc].x;
+                oldThisY = this.containers[cc].y;
+            }
+        });
+
+        this.element.addEventListener('mouseup', () => {
+            isDown = false;
+        });
+
+        this.element.addEventListener('mousemove', (event) => {
+
+            const cc2 = this.containers.findIndex((container) => {
+                return (event.x >= container.x && event.x <= container.x + container.width) &&
+                    (event.y >= container.y && event.y <= container.y + container.height);
+            });
+
+            if (cc2 !== -1) {
+                document.body.style.cursor = 'pointer';
+                this.containers[cc2].active = true;
+            } else {
+                document.body.style.cursor = 'default';
+                this.containers[cc2].active = false;
+            }
+
+            if (isDown && cc !== -1) {
+                const mouseXOffset = oldMouseX - event.x;
+                const mouseYOffset = oldMouseY - event.y;
+
+                this.containers[cc].x = oldThisX - mouseXOffset;
+                this.containers[cc].y = oldThisY - mouseYOffset;
+            }
         });
     }
 
@@ -42,12 +94,12 @@ export class App {
     }
 
     private drawBackground() {
-        this.ctx.fillRect(0, 0, 1920, 1080)
+        this.ctx.fillRect(0, 0, 1920, 1080);
     }
 
     private draw() {
-        this.ctx.fillStyle = 'black';
-        this.ctx.fillRect(0, 0, this.element.width, this.element.height)
+        this.ctx.fillStyle = '#1E1F23';
+        this.ctx.fillRect(0, 0, this.element.width, this.element.height);
 
         this.containers.forEach((container) => {
             container.update();
@@ -63,6 +115,6 @@ export class App {
 
     private showFPS() {
         this.ctx.fillStyle = 'white';
-        this.ctx.fillText(`FPS: ${this.FPSFinal}`, 10, this.element.height - 10)
+        this.ctx.fillText(`FPS: ${this.FPSFinal}`, 10, this.element.height - 10);
     }
 }
--- src/core/Container.ts
+++ src/core/Container.ts
@@ -4,6 +4,7 @@ export class Container {
     public y: number;
     public width: number;
     public height: number;
+    public active: boolean;
     public ctx: CanvasRenderingContext2D;
 
     constructor(x: number, y: number, width: number, height: number, ctx: CanvasRenderingContext2D) {
@@ -12,41 +13,7 @@ export class Container {
         this.width = width;
         this.height = height;
         this.ctx = ctx;
-
-        let isDown = false;
-        let oldMouseX = 0;
-        let oldMouseY = 0;
-        let oldThisX = 0;
-        let oldThisY = 0;
-
-        document.addEventListener('mousedown', (event) => {
-
-            if (
-                (event.x >= this.x && event.x <= this.x + this.width) &&
-                (event.y >= this.y && event.y <= this.y + this.height)
-            ) {
-                isDown = true;
-                oldMouseX = event.x;
-                oldMouseY = event.y;
-
-                oldThisX = this.x;
-                oldThisY = this.y;
-            }
-        });
-
-        document.addEventListener('mouseup', () => {
-            isDown = false;
-        });
-
-        document.addEventListener('mousemove', (event) => {
-            if (isDown) {
-                const mouseXOffset = oldMouseX - event.x;
-                const mouseYOffset= oldMouseY - event.y;
-
-                this.x = oldThisX - mouseXOffset;
-                this.y = oldThisY - mouseYOffset;
-            }
-        })
+        this.active = false;
     }
 
     update() {
@@ -55,8 +22,15 @@ export class Container {
 
     draw() {
         this.ctx.beginPath();
-        this.ctx.fillStyle = 'white';
+        this.ctx.fillStyle = this.active ? '#30D094' : 'white';
         this.ctx.fillRect(this.x, this.y, this.width, this.height);
+        this.ctx.closePath();
+
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.x, this.y);
+        this.ctx.lineTo(this.x + this.width, this.y + this.height);
+        this.ctx.strokeStyle = 'red';
+        this.ctx.stroke();
         this.ctx.closePath();
     }
 }
